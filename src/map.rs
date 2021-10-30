6ffaa0f29299167898eca694ac0fb582fec5b866
Daniel Hampton <dhampton084@gmail.com>
2021-10-30 18:48:52 -0400

    add player

--- src/map.rs
+++ src/map.rs
@@ -30,6 +30,25 @@ impl Map {
             }
         }
     }
+
+    /// Determine if player is within the map bounds.
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    /// Determine if the player can enter the tile or is at the bounds of the
+    /// map.
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if self.in_bounds(point) {
+            Some(map_idx(point.x, point.y))
+        } else {
+            None
+        }
+    }
 }
 
 pub fn map_idx(x: i32, y: i32) -> usize {
